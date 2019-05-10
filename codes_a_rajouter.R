9b17aee7a383fdfbd3e80444ed129cbf106410d8
Benjamin Landré <31775498+BenjamLandre@users.noreply.github.com>
2019-05-10 17:46:54 +0200

    Add files via upload

---
+++ codes_a_rajouter.R
@@ -0,0 +1,33 @@
+#######
+#basicbuilding
+
+# dans le truc vecteur de données
+
+# calculs entre vecteurs
+
+vecteur_a <- c(12, 45, 18)
+
+vecteur_a + 1
+vecteur_a + vecteur_a
+
+Plus_long_que_a <- c(1, 2, 3, 4, 5)
+Deux_fois_plus_long_que_a <- c(1, 2, 3, 4, 5, 6)
+
+vecteur_a + Plus_long_que_a
+vecteur_a + Deux_fois_plus_long_que_a
+
+vecteur_a + Plus_long_que_a + Deux_fois_plus_long_que_a
+
+c(12, 45, 18, 12, 45, 12) + c(1, 2, 3, 4, 5, 1) + c(1, 2, 3, 4, 5, 6) 
+# il fait vecteur_a + Plus_long_que_a puis il rajotue le 3ème, ce qui réinitialise le recyclage
+# conclusion = casse gueule; mieux vaut ajouter des vecteurs de mêmes tailles ou multiples simples
+
+Poids <- c(60, 65, 70)
+Taille <- c(1.70, 1.72, 1.74)
+Taille_au_carre <- #
+IMC <- #
+IMC
+
+###
+#
+######
